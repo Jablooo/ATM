4fd8d123c3e04a2fb9fe3d0cdbd27d88cdf59a20
Jon Ablondi <jonnyablondi@hotmail.com>
2017-08-06 09:48:17 +1000

    adding loops and options

--- atm.rb
+++ atm.rb
@@ -12,68 +12,75 @@ add an array to track the account @
 
 =end
 
+# Introduction of user interface
+puts
+puts
+puts
+puts
+puts
+puts
+puts "       Welcome to Jon Bank"
+puts "     Please insert your card"
+puts "     press enter to continue"
+puts
+puts
+puts
+puts
+
+gets.chomp
+
 def intro
   #Establish the account balance
   $balance = 0
-
-  # Introduction of user interface
-  puts
-  puts
-  puts "       Welcome to Jon Bank"
-  puts "     Please insert your card"
-  puts "     press enter to continue"
-
-  gets.chomp
+  $runner = 1
 
   puts
   puts
   puts "   Thank you for banking with us today."
-  puts "     Here are your available options"
-  puts
-  puts "[1] Withdraw             [3] Check Balance"
-  puts "[2] Deposit              [4] Remove Card"
-  puts
-  puts
+  while $runner == 1
+    puts "     Here are your available options"
+    puts
+    puts "[1] Withdraw             [3] Check Balance"
+    puts "[2] Deposit              [4] Remove Card"
+    puts
+    puts
 
-runner = 1
-
-  while runner == 1
     optionsAnswer = gets.chomp.to_i
 
     if optionsAnswer == 1
       withdraw_function
-      puts $balance
     elsif optionsAnswer == 2
       deposit_function
-      puts $balance
     elsif optionsAnswer == 3
       check_balance
     elsif optionsAnswer == 4
+      puts
+      puts
       puts "We hope you have enjoyed your Jon Bank"
       puts " experience and hope to see you soon"
+      puts
+      puts
 
-      runner = 2
+      $runner = 2
+      return $runner
     else
       puts "Please select valid option"
     end
   end
-end
 
-def withdraw_function
-  puts  "How much would you like to withdraw today?"
-  withdrawAnswer = gets.chomp.to_i
+  while $runner ==2
 
-  if withdrawAnswer <= 0
-    puts "Please write a positive number"
-  elsif withdrawAnswer < 0 && withdrawAnswer < $balance
-  $balance = $balance - withdrawAnswer
+    puts
+    puts
+    puts
+    puts
+    puts "We hope you have enjoyed your Jon Bank"
+    puts " experience and hope to see you soon"
+    puts
+    puts
+    puts
+    puts
 
-  puts "Your balance is now #{$balance}"
-  puts "Have a nice day"
-  return $balance
-
-  else
-    puts "You have insufficient funds"
   end
 end
 
@@ -85,19 +92,126 @@ def deposit_function
     puts "Please write a positive number"
   elsif depositAnswer > 0
   $balance = $balance + depositAnswer
+  puts
+  puts
+  puts
+  puts
+  puts
+  puts
+  puts "Your balance is now $#{$balance}"
+  puts
+  puts
+  puts
+  puts
+  puts
+  puts
+  puts "Would you like to do anything else?"
+  puts "             [Y]/[N]"
+  puts
+  puts
+  puts
+  puts
+  option = gets.chomp.downcase
+    if option == "y"
+      $runner = 1
+      return $runner
+    elsif option == "n"
+      $runner = 2
+      return $runner
+    else puts "Please select valid option"
+    end
 
-  puts "Your balance is now #{$balance}"
-  puts "Have a nice day"
   return $balance
 
   else
+  puts
+  puts
+  puts
+  puts
   puts "Please enter number"
+  puts
+  puts
+  puts
+  puts
+  end
+end
+
+def withdraw_function
+  puts  "How much would you like to withdraw today?"
+  withdrawAnswer = gets.chomp.to_i
+
+  if withdrawAnswer <= 0
+    puts "Please write a positive number"
+  elsif withdrawAnswer > 0 && withdrawAnswer < $balance
+  $balance = $balance - withdrawAnswer
+  puts
+  puts
+  puts
+  puts
+  puts
+  puts
+  puts "Your balance is now $#{$balance}"
+  puts
+  puts
+  puts
+  puts
+  puts
+  puts
+  puts "Would you like to do anything else?"
+  puts "             [Y]/[N]"
+  puts
+  puts
+  puts
+  puts
+  option = gets.chomp.downcase
+    if option == "y"
+      $runner = 1
+      return $runner
+    elsif option == "n"
+      $runner = 2
+      return $runner
+    else puts "Please select valid option"
+    end
+
+  return $balance
+
+  else
+    puts
+    puts
+    puts
+    puts
+    puts "You have insufficient funds"
+    puts
+    puts
+    puts
+    puts
   end
 end
 
 def check_balance
-  puts "Your balance is #{$balance}"
-  puts "Have a nice day"
+  puts
+  puts
+  puts
+  puts
+  puts "Your balance is $#{$balance}"
+  puts
+  puts
+  puts
+  puts "Would you like to do anything else?"
+  puts "             [Y]/[N]"
+  puts
+  puts
+  puts
+  puts
+  option = gets.chomp.downcase
+  if option == "y"
+    $runner = 1
+    return $runner
+  elsif option == "n"
+    $runner = 2
+    return $runner
+  else puts "Please select valid option"
+  end
 end
 
 intro
